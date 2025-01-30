e62fcb2ab9b13a370f2f63b9fc406b995dae624f
dannylee73lee <dannylee73@gmail.com>
2025-01-30 08:28:49 +0000

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from sklearn import *
+import xgboost as xgb
+import lightgbm as lgb
+import geopandas as gpd
+import folium
+import networkx as nx
+import mapclassify
+from pyproj import *
+from shapely.geometry import *
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# 페이지 기본 설정
+st.set_page_config(
+    page_title="My Streamlit App",
+    page_icon="🌟",
+    layout="wide"
 )
+
+# 앱 제목
+st.title("My Data Analysis App")
